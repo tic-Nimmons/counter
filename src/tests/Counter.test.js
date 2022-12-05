652f48f77adea38cc39d9c535d592b09a1eb2e67
tic-Nimmons <n.timmons@ufl.edu>
2022-12-04 20:36:28 -0500

    Updated Counter Test

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -2,21 +2,35 @@
 // import the Counter component here
 
 beforeEach(() => {
-  // Render the Counter component here
+  //Begin by rendering the counter component before each individual test case
+  render(<Counter />)
 })
 
 test('renders counter message', () => {
   // Complete the unit test below based on the objective in the line above
+  const counterMessage = screen.getByText(/Counter/i);
+  expect(counterMessage).tobeInDocument();
 });
 
 test('should render initial count with value of 0', () => {
   // Complete the unit test below based on the objective in the line above
+  const initial = screen.getByTestId('count');
+  const result = '0';
+  expect(initial).toHaveTextContent(result);
 });
 
 test('clicking + increments the count', () => {
   // Complete the unit test below based on the objective in the line above
+  const initial = screen.getByTestId('count');
+  const result = '1';
+  userEvent.click(screen.getByText('+');
+  expect(initial).toHaveTextContent(result);
 });
 
 test('clicking - decrements the count', () => {
   // Complete the unit test below based on the objective in the line above
+  const initial = screen.getByTestId('count');
+  const result = '-1';
+  userEvent.click(screen.getByText('-'));
+  expect(initial).toHaveTextContent(result);
 });
